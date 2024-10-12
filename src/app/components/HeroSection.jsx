b1d8420353059586177dcb88ecd075688057bd53
carlos-22-chasi <c071503s@gmail.com>
2024-10-12 18:56:02 -0500

    created hero section

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,59 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hello, I&#39;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'Carlos',
+                2000, // wait 1s before replacing "Mice" with "Hamsters"
+                'Hispanic',
+                2000,
+              ]}
+              wrapper="span"
+              speed={50}
+              // style={{ fontSize: '2em', display: 'inline-block' }}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className='text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6'>
+            balch lfja;jkv;lakjd fl;adsjfdsj asldjkf ;alfjkd 
+            as f
+          </p>
+          <div>
+            <button className='px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-green-300 via-green-500 to-green-700 hover:bg-slate-100 text-white'>Hire Me</button>
+            <button className='px-1 py-1 rounded-full w-full sm:w-fit  bg-gradient-to-br from-green-300 via-green-500 to-green-700 hover:bg-slate-800 text-white mt-3'>
+              <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
+              Download CV
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mg-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src="/images/hero_pic.png"
+              alt='hero image'
+              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+              width={300}
+              height={300}
+              />
+          </div>
+        </div>
+      </div>
+    </section>  
+  )
+}
+
+export default HeroSection
