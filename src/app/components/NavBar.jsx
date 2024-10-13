416a5193d4868828cdba2d2e4fbc46ac93a2d017
carlos-22-chasi <c071503s@gmail.com>
2024-10-13 16:41:56 -0500

    fixed z positioning issue

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
   const[navbarOpen, setNavBarOpen] = useState(false);
 
   return (
-    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
+    <nav className='fixed top-0 left-0 right-0 z-20 bg-[#121212] bg-opacity-100'>
       <div className='flex flex-wrap items-center justify-between mx-auto px-4 py-3'>
         <Link href="/" className='text-2xl md:text-5xl font-semibold text-white'>
         LOGO
