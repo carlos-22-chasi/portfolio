ee49e90c9728f64a38ec61339ffb4ce4791a7304
carlos-22-chasi <c071503s@gmail.com>
2024-10-14 10:28:06 -0500

    added functionality for links

--- src/app/components/ContactSection.jsx
+++ src/app/components/ContactSection.jsx
@@ -42,6 +42,7 @@ const ContactSection = () => {
 
   return (
     <section id="contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+
       {/* left side of contact section */}
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute left-36 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
@@ -54,15 +55,23 @@ const ContactSection = () => {
           open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
         </p>
-        <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
-            <Image src={GithubIcon} alt="Github Icon" className="w-12"/>
+
+        <div className="socials flex flex-row gap-2 mt-10">
+          <Link href="https://github.com/carlos-22-chasi" target="_blank" rel="noopener noreferrer" >
+            <Image 
+              src={GithubIcon} 
+              alt="Github Icon" 
+              className="w-12 transition-transform duration-100 ease-in-out hover:-translate-y-2 active:rotate-180"/>
           </Link>
-          <Link href="linkedin.com">
-            <Image src={LinkedinIcon} alt="Linkedin Icon" className="w-14"/>
+          <Link href="https://www.linkedin.com/in/carlos-c-18ab47230" target="_blank" rel="noopener noreferrer" >
+            <Image 
+              src={LinkedinIcon} 
+              alt="Linkedin Icon" 
+              className="w-14 transition-transform duration-100 ease-in-out hover:-translate-y-2 active:rotate-180"/>
           </Link>
         </div>
       </div>
+
       {/* right side of contact section */}
       <div>
         {emailSubmitted ? (
